6aa52f7f233560a5c02234004e66f213f92c12c8
AndreiR <andreivradulescu@gmail.com>
2024-04-30 11:18:48 +0300

    [ DEBUG ] mutat functia de creare dinamica categorii in functions.php pt debug

--- functions.php
+++ functions.php
@@ -64,35 +64,58 @@ endif;
 
 add_action('wp_enqueue_scripts', 'plumbing_fse_child_enqueue_scripts');
 
-
-
-// Inserare in db categorii produse
-function create_product_category($category_name)
+function create_product_category()
 {
-    $term = wp_insert_term(
-        $category_name,   // nume
-        'product_cat',    // taxonomie (categorii produse)
-        array(
-            'description' => '',  // descriere
-            'slug' => sanitize_title($category_name)  // slug categorie
-        )
-    );
-    if (is_wp_error($term)) {
-        echo 'Eroare creare categorie: ' . $category_name . '. Error: ' . $term->get_error_message();
-    } else {
-        echo 'Categorie produs inregistrata cu succes: ' . $category_name . '<br>';
+    try {
+        $cats = array(
+            array('thumb' => '/assets/img/categ_icons/central-heating.png', 'name' => 'centrale-termice-in-condensare', 'description' => 'descriere categorie centrale', 'slug' => 'centrale-termice-in-condensare-BAXI', 'parent_categ_id' => ''),
+            array('thumb' => '/assets/img/categ_icons/central-heating.png', 'name' => 'boilere', 'description' => 'descriere categorie centrale', 'slug' => 'boilere', 'parent_categ_id' => 0),
+            array('thumb' => '/assets/img/categ_icons/central-heating.png', 'name' => 'radiatoare', 'description' => 'descriere categorie centrale', 'slug' => 'radiatoare', 'parent_categ_id' => 0),
+        );
+
+        foreach ($cats as $data) {
+            $thumb_id = get_attachment_id_from_url($data['thumb']);
+
+            $cat_args = array(
+                'description' => $data['description'],
+                'slug' => $data['slug'],
+                'parent' => $data['parent_categ_id']
+            );
+
+            $cat_id = wp_insert_term($data['name'], 'product_cat', $cat_args);
+            var_dump($cat_id);
+
+            if (!is_wp_error($cat_id) && isset($cat_id['term_id'])) {
+                // Log success message
+                error_log('Category created successfully. Term ID: ' . $cat_id['term_id']);
+
+                // Set thumbnail for category
+                update_term_meta($cat_id['term_id'], 'thumbnail_id', $thumb_id);
+
+                // Log thumbnail assignment
+                error_log('Thumbnail assigned to category. Thumbnail ID: ' . $thumb_id);
+            } else {
+                // Log error message
+                if (is_wp_error($cat_id)) {
+                    error_log('Error creating category: ' . $cat_id->get_error_message());
+                } else {
+                    error_log('Unknown error occurred while creating category.');
+                }
+            }
+        }
+    } catch (Exception $e) {
+        // Log any exceptions
+        error_log('Exception caught: ' . $e->getMessage());
     }
 }
 
-$categories = array(
-    'centrale-termice-in-condensare-BAXI',
-    'boilere',
-    'radiatoare',
-    'incalzire-in-pardoseala',
-);
-
-foreach ($categories as $category) {
-    create_product_category($category);
+// Function to get attachment ID from URL
+function get_attachment_id_from_url($url)
+{
+    $attachment_id = 0;
+    $attachment_url = esc_url($url);
+    $attachment_id = attachment_url_to_postid($attachment_url);
+    return $attachment_id;
 }
 
-add_action('after_setup_theme', 'create_product_category');
+create_product_category();
